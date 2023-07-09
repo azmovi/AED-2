d3d9dc697ef40c2b9270942cee1906b809a8673c
azevedo <azevedoantoniocicero@gmail.com>
2023-07-09 19:17:46 -0300

    fazendo a remoção de no

--- AVL-Tree/tree.py
+++ AVL-Tree/tree.py
@@ -15,10 +15,15 @@ class Avl():
     def preorder(self, no: Node = None):
         if no == None:
             return 
-        print(no.value, self.fator_balanceamento(no))
+        print(no.value, self.get_pai(no) )
         self.preorder(no.left)
         self.preorder(no.right)
         return
+
+    def get_pai(self, no:Node):
+        if no.pai == None:
+            return None
+        return no.pai.value
 
     def insert(self, no:Node):
         x = self.root
@@ -40,32 +45,111 @@ class Avl():
 
         self.tree_altura_update(no)
 
-        """while no != None:
+
+        novo_no = no
+        self.rebalanceamento(no, novo_no)
+
+        return
+
+    def rebalanceamento(self, no:Node, novo_no:Node):
+        while no != None:
+            self.update_altura(no)
+            fb = self.fator_balanceamento(no)
+            if fb not in [-1, 0, 1]:
+                self.rotation_insert(no, novo_no, fb)
+            no = no.pai
+        return
+
+    def rotation_insert(self, no:Node, novo_no:Node, fb:int):
+        if fb > 1:
+            if novo_no.value <= no.left.value:
+                self.right_rotate(no)
+                return 
+            else:
+                #rotaçao esquerda direita
+                xpto = no.left
+                self.left_rotate(xpto)
+                self.right_rotate(no)
+                return 
+        if fb < -1:
+            if novo_no.value > no.right.value:
+                self.left_rotate(no)
+                return
+            else:
+                xpto = no.right
+                self.right_rotate(xpto)
+                self.left_rotate(no)
+                return
+        return
+
+    def deletion(self, no: Node) -> bool:
+        raiz = False
+
+        if no != None:
+            return False
+
+        if no.left != None:
+            filho_esquerdo = no.left
+        if no.right != None:
+            filho_direito = no.right
+
+        if no.pai != None:
+            no_pai = no.pai
+
+        else:
+            raiz = True
+
+        #caso 1 -> No folha
+        if no.left == None and no.right == None:
+            if no.pai == None:
+                self.raiz = None
+                return True
+
+            else:
+                del(no)
+                self.rebalanceamento_del(no_pai)
+                return True
+
+            return False
+
+        return
+
+    def rebalanceamento_del(self, no):
+        while no != None:
+            self.update_altura(no)
             fb = self.fator_balanceamento(no)
             if fb not in [1, 0, -1]:
-                self.rotation(no, fb, novo_no)
-            no = no.pai"""
-
-
+                no = no.pai
         return
+    
+    def rotaçao_delete(self, no, fb):
 
-    def rotation(self, no:Node, fb: int, novo_no:Node):
+        if no.left != None:
+            fb_left = self.fator_balanceamento(no.left)
+
         if fb > 1:
-            if no.left != None and novo_no.value <= no.left.value:
-                self.right_rotate(no)
-            else:
-                self.left_right_rotate(no)
-            return
-        if fb < -1:
-            if no.right != None and novo_no.value > no.right.value:
+            if no.left and fb_left >= 0:
                 self.left_rotate(no)
+                return
+
             else:
-                self.right_left_rotate(no)
+                xpto = self.left_rotate(no)
+                self.right_rotate(xpto)
+                return
+
             return
+
+        if fb <= -1:
+            if no.left and fb_left <= 0:
+                self.left_rotate(no)
+                return
+            else:
+                xpto = self.right_rotate(no)
+                self.left_rotate(xpto)
+                return
+            return
+
         return
-
-
-
 
     def update_altura(self, no: Node) -> Node:
         if no == None:
@@ -95,66 +179,69 @@ class Avl():
 
     def left_rotate(self, x: Node):
         y = x.right
+        if y == None:
+            return
         sub = y.left
-        x.right = sub 
+
         y.left = x
-
-        self.tree_altura_update(y)
-        self.tree_altura_update(x)
-
-        y.pai = x.pai
-        x.pai = y
+        x.right = sub
 
         if sub != None:
             sub.pai = x
 
-        return y
+        y.pai = x.pai
+        if x.pai == None:
+            self.root = y
+        elif x == x.pai.left:
+            x.pai.left = y
+        else:
+            x.pai.right = y
+
+        x.pai = y
+
+        self.update_altura(y)
+        self.update_altura(x)
+
+        return
 
     def right_rotate(self, y: Node):
         x = y.left
-        sub = x.right
-        y.left = sub
-        x.right = y
+        if x == None:
+            return
+        sub = x.right  
 
-        self.tree_altura_update(x)
-        self.tree_altura_update(y)
+        x.right = y
+        y.left = sub
+        if sub != None:
+            sub.pai = x
 
         x.pai = y.pai
+        if y.pai == None:
+            self.root = x
+        elif y == y.pai.left:
+            y.pai.left = x
+        else:
+            y.pai.right = x
+
         y.pai = x
+        self.update_altura(y)
+        self.update_altura(x)
 
-        if sub != None:
-            sub.pai = y
-
-        return x
-
-    def left_right_rotate(self, z: Node):
-        x = z.left
-        y = self.left_rotate(x)
-        w = self.right_rotate(y)
-
-        self.tree_altura_update(z)
-        return w
-
-    def right_left_rotate(self, z: Node):
-        x = z.right
-        y = self.right_rotate(x)
-        w = self.left_rotate(y)
-
-        self.tree_altura_update(z)
-        return w
-
+        return
 
 
 
 arvore = Avl()
 arvore.insert(Node(33))
 arvore.insert(Node(13))
+arvore.insert(Node(11))
+no21 = Node(21)
+arvore.insert(no21)
 arvore.insert(Node(53))
 arvore.insert(Node(61))
-arvore.insert(Node(21))
-arvore.insert(Node(11))
-no8 = Node(8)
-arvore.insert(no8)
+arvore.insert(Node(8))
 arvore.insert(Node(9))
-#arvore.left_rotate(no8)
+
+arvore.deletion(no21)
+
 arvore.preorder(arvore.root)
